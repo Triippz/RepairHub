5e93f914926ce715dd817788e342f54798b30181
Mark Tripoli <mtripoli@hubspot.com>
2023-08-14 16:00:53 -0400

    port env

--- scheduler-api/src/main.ts
+++ scheduler-api/src/main.ts
@@ -23,7 +23,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("swagger-ui", app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
   Logger.log(
       `🚀 Application is running on: http://localhost:${3000}`
   );
