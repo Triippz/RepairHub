c38dba1a42939a66ba921e0395432e3b27a4ba64
Mark Tripoli <mtripoli@hubspot.com>
2023-08-30 10:43:11 -0400

    not really api keys

---
+++ scheduler-api/prisma/migrations/20230829151403_added_api_keys/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `ApiKey` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `key` VARCHAR(191) NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `ApiKey_key_key`(`key`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ApiKey` ADD CONSTRAINT `ApiKey_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- scheduler-api/src/auth/auth.module.ts
+++ scheduler-api/src/auth/auth.module.ts
@@ -1,26 +1,25 @@
-import {Module} from "@nestjs/common";
-import {AuthService} from "./auth.service";
-import {AuthController} from "./auth.controller";
-import {PrismaService} from "nestjs-prisma";
-import {PassportModule} from "@nestjs/passport";
-import {UsersModule} from "../users/users.module";
-import { JwtStrategy } from './jwt.strategy';
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PrismaService } from 'nestjs-prisma';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
-import config from "../config";
-
+import config from '../config';
 
 @Module({
-    imports: [
-        UsersModule,
-        PassportModule.register({ defaultStrategy: 'jwt' }),
-        JwtModule.register({
-            secret: config.jwt.secretOrKey,
-            signOptions: {
-                expiresIn: config.jwt.expiresIn,
-            },
-        }),
-    ],
-    providers: [AuthService, JwtStrategy, PrismaService],
-    controllers: [AuthController],
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: config.jwt.secretOrKey,
+      signOptions: {
+        expiresIn: config.jwt.expiresIn,
+      },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy, PrismaService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
